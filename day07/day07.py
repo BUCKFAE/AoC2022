9d5940abb5c786b34d2866e4dddc867ef7bede47
Julian Schubert <informatik.schubert@gmail.com>
2022-12-08 11:04:28 +0100

    Day07 and Start Day08

--- day07/day07.py
+++ day07/day07.py
@@ -1,28 +1,165 @@
 from __future__ import annotations
 
 import os
+from dataclasses import dataclass
 
+
+@dataclass
+class directory:
+    name: str
+    parent: directory | None
+    folders: dict[str, directory]
+
+    # File: name, size
+    files: list[tuple[str, int]]
+    size: int | None
+
+
+def get_dir_size(dir: directory) -> int:
+
+    # If size was already calculated, return it
+    if dir.size:
+        return dir.size
+
+    to_handle = [dir]
+
+    size = 0
+
+    while to_handle:
+        current = to_handle.pop()
+        current_files_size = sum([size for _, size in current.files])
+        current_folders_size = sum([get_dir_size(folder) for folder in current.folders.values()])
+
+        size += current_files_size + current_folders_size
+
+    # Caching the size
+    dir.size = size
+
+    return size
+
+def find_dir(root: directory, name: str) -> directory:
+    if root.name == name:
+        return root
+
+    for folder in root.folders.values():
+        return find_dir(folder, name)
+
+    raise ValueError(f'Could not find directory {name}')
+
+def iterate_dirs(root: directory):
+    yield root
+
+    for folder in root.folders.values():
+        yield from iterate_dirs(folder)
+
+def create_directory_tree(input_file: str) -> directory:
+    assert os.path.exists(input_file), f'File {input_file} does not exist'
+
+    root = directory('/', None, {}, [], None)
+
+    with open(input_file, 'r') as f:
+        commands = [line.strip() for line in f.read().splitlines() if line.strip()]
+
+        assert commands[0] == '$ cd /', f'Expected to start in the root dir'
+
+        current_dir = root
+
+        idx = 1
+        while idx < len(commands):
+            command = commands[idx]
+            print(f'Command {idx}: {command}')
+            if command.startswith('$ cd '):
+                # Change directory
+                tgt = command.removeprefix('$ cd ')
+                print(f'Target: {tgt}')
+
+                if tgt == '..':
+                    # Go up one level
+                    assert current_dir.parent is not None, f'Cannot go up from root'
+                    current_dir = current_dir.parent
+                else:
+                    # Checking if we already know the folder
+                    known_folder = tgt in current_dir.folders.keys()
+                    print(f'Folder is known: {known_folder}')
+
+                    if known_folder:
+                        current_dir = current_dir.folders[tgt]
+                    else:
+                        # Create new folder
+                        new_dir = directory(tgt, current_dir, {}, [], None)
+                        current_dir.folders[tgt] = new_dir
+                        current_dir = new_dir
+
+                # Going to the next instruction
+                idx += 1
+
+            elif command.startswith('$ ls'):
+                # List directory
+                next_command_index = [i for i in range(idx + 1, len(commands)) if commands[i].startswith('$')]
+
+                # Last command
+                if not next_command_index:
+                    next_command_index = len(commands)
+                else:
+                    # Not last command
+                    next_command_index = next_command_index[0]
+                    print(f'Next command: {commands[next_command_index]} - {next_command_index}')
+
+                for entry in range(idx + 1, next_command_index):
+                    content = commands[entry]
+                    if content.startswith('dir '):
+                        # Directory
+                        dir_name = content.removeprefix('dir ')
+                        assert dir_name not in current_dir.folders.keys(), f'Directory {dir_name} already exists'
+                        print(f'Found dir: {dir_name}')
+                        current_dir.folders[dir_name] = directory(dir_name, current_dir, {}, [], None)
+                    else:
+                        size, name = content.split(' ')
+                        size = int(size)
+                        print(f'Found file: {name} - {size}')
+                        current_dir.files.append((name, size))
+
+                # Going to the next instruction
+                idx = next_command_index
+            else:
+                # Unknown command
+                raise ValueError(f'Unknown command: {command} in line {idx})')
+    return root
 
 def solve(input_file: str)-> tuple[int, int]:
 
-    assert os.path.exists(input_file), f'File {input_file} does not exist'
-
-    with open(input_file, 'r') as f:
-        file_text = f.read().splitlines()
-
     res1 = 0
+    root = create_directory_tree(input_file)
+    for dir in iterate_dirs(root):
+        size = get_dir_size(dir)
+        if size <= 100_000:
+            res1 += size
+
     res2 = 0
+
+    disk_space = 70_000_000
+    total_space_required = 30_000_000
+    current_space = get_dir_size(root)
+    free_space = disk_space - current_space
+    missing_space_required = total_space_required - free_space
+
+    res2 = min([d for d in iterate_dirs(root) if d.size >= missing_space_required ], key=lambda d: d.size).size
 
     return res1, res2
 
 
 if __name__ == '__main__':
 
-    # Solving sample inputs
-    res_sample_1 = solve('day07/sample_inputs/sample_input_1.txt')
-    #assert res_sample_1[0] == 7, f'Expected 7, got {res_sample_1[0]}'
-    #assert res_sample_1[1] == 19, f'Expected 19, got {res_sample_1[1]}'
 
-    # Solving real input
+    # Testing size of directories
+    root1 = create_directory_tree('day07/sample_input.txt')
+    assert get_dir_size(root1) == 48381165, f'Expected 48381165, got {get_dir_size(root1)}'
+    assert get_dir_size(find_dir(root1, 'e')) == 584, f'Expected 584, got {get_dir_size(find_dir(root1, "e"))}'
+
+    res_sample = solve('day07/sample_input.txt')
+
+    assert res_sample[0] == 95437, f'Expected 95437, got {res_sample[0]}'
+    assert res_sample[1] == 24933642, f'Expected 24933642, got {res_sample[1]}'
+
     res = solve('day07/input.txt')
     print(f'Part 1: {res[0]} - Part 2: {res[1]}')
---
+++ day08/day08.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+import os
+
+import numpy as np
+
+
+class Grid:
+    def __init__(self, data: np.ndarray):
+        self.data: np.ndarray = data
+
+    def get_neighbors(self, x: int, y:int) -> list[int]:
+        # Get all tiles up / left / right / down
+        neighbors = []
+        if x < self.data[0].size - 1:
+            neighbors.append(self.data[y, x + 1])
+        if x > 0:
+            neighbors.append(self.data[y, x - 1])
+        if y < self.data[1].size - 1:
+            neighbors.append(self.data[y + 1, x])
+        if y > 0:
+            neighbors.append(self.data[y - 1, x])
+        return neighbors
+
+    def get_height(self, x: int, y: int) -> int:
+        return self.data[y, x]
+
+    def is_visible(self, x: int, y: int) -> bool:
+
+        own_height = self.get_height(x, y)
+        directions: list[tuple[int, int]] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for dir in directions:
+            current = (x, y)
+
+            visible = True
+            while len(self.get_neighbors(current[0], current[1])) > 3:
+                current = (current[0] + dir[0], current[1] + dir[1])
+
+                if self.get_height(current[0], current[1]) >= own_height:
+                    visible = False
+                    break
+            if visible:
+                return True
+        return False
+
+    def get_tree_score(self, x: int, y: int) -> int:
+        own_height = self.get_height(x, y)
+        directions: list[tuple[int, int]] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        score = 1
+
+        for dir in directions:
+            current = (x, y)
+
+            dir_score = 0
+            while len(self.get_neighbors(current[0], current[1])) > 3:
+                current = (current[0] + dir[0], current[1] + dir[1])
+
+                if own_height > self.get_height(current[0], current[1]):
+                    dir_score += 1
+                else:
+                    break
+            print(f'Direction {dir} has score {dir_score}')
+            score *= max(1, dir_score)
+        return score
+
+    def get_all_coordinates(self) -> list[tuple[int, int]]:
+        return [(x, y) for x in range(self.data[0].size) for y in range(self.data[1].size)]
+
+
+
+def create_grid(input_file: str) -> Grid:
+    assert os.path.exists(input_file), f'File {input_file} does not exist'
+    with open(input_file, 'r') as f:
+        arr = np.array([[int(n) for n in line.strip()] for line in f.read().splitlines() if line.strip()])
+    return Grid(arr)
+
+def solve(input_file: str)-> tuple[int, int]:
+
+    grid = create_grid(input_file)
+
+
+    res1 = sum([1 if grid.is_visible(x, y) else 0 for x, y in grid.get_all_coordinates()])
+
+    best_tree = max(grid.get_all_coordinates(), key=lambda x: grid.get_tree_score(x[0], x[1]))
+    res2 = grid.get_tree_score(best_tree[0], best_tree[1])
+
+
+    return res1, res2
+
+
+if __name__ == '__main__':
+
+    sample_grid = create_grid('day08/sample_input.txt')
+
+    print(sample_grid.get_all_coordinates())
+
+    # Getting neighbors
+    assert sorted(sample_grid.get_neighbors(0, 0)) == [0, 2]
+    assert sorted(sample_grid.get_neighbors(0, 1)) == [3, 5, 6]
+    assert sorted(sample_grid.get_neighbors(4, 4)) == [9, 9]
+
+    # Visibility
+    assert sample_grid.is_visible(1, 1)
+    assert not sample_grid.is_visible(2, 2)
+    assert not sample_grid.is_visible(1, 3)
+    assert sample_grid.is_visible(2, 3)
+    assert not sample_grid.is_visible(3, 3)
+
+    # Tree score
+    assert sample_grid.get_tree_score(2, 1) == 4, f'Expected 4, got {sample_grid.get_tree_score(2, 1)}'
+
+    # Solving sample inputs
+    res_sample = solve('day08/sample_input.txt')
+    assert res_sample[0] == 21, f'Expected 21, got {res_sample[0]}'
+    assert res_sample[1] == 8, f'Expected 8, got {res_sample[1]}'
+
+    # Solving real input
+    res = solve('day08/input.txt')
+    print(f'Part 1: {res[0]} - Part 2: {res[1]}')
